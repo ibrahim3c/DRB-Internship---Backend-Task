d8e1e6c91cd34351e8211e75abd8a81c61c7626f
ibrahim hany <ihany941@gmail.com>
2025-09-22 21:13:17 +0300

    Refactor app.js to organize routes and enhance error handling

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ import helmet from "helmet";
 import morgan from "morgan";
 import dotenv from "dotenv";
 
-// import userRoutes from "./routes/userRoutes";
-// import errorMiddleware from "./middlewares/errorMiddleware";
+import driverRoutes from "./routes/driverRoutes.js";
+import routeRoutes from "./routes/routeRoutes.js";
 
 dotenv.config();
 const app = express();
@@ -17,9 +17,30 @@ app.use(helmet()); // more security
 app.use(morgan("dev")); // for logging
 
 // Routes
-// app.use("/api/users", userRoutes);
+app.get("/api", (req, res) => res.send("Route Scheduling System API"));
+app.use("/api/drivers", driverRoutes);
+app.use("/api/routes", routeRoutes);
 
-// Error Handling
-// app.use(errorMiddleware);
+// global middleware for not found router
+app.all("*", (req, res, next) => {
+  return res
+    .status(404)
+    .json({
+      status: httpStatusText.ERROR,
+      message: "this resource is not available",
+    });
+});
+
+// global error handler
+app.use((error, req, res, next) => {
+  res
+    .status(error.statusCode || 500)
+    .json({
+      status: error.statusText || httpStatusText.ERROR,
+      message: error.message,
+      code: error.statusCode || 500,
+      data: null,
+    });
+});
 
 export default app;
